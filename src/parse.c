bcc5f8d595c1ebba04bc9bbd82381c24fcc11d66
Daylon Williams <daylonjwilliams@gmail.com>
2025-10-08 06:50:33 -0400

    Change malloc to check for NULL instead of -1

--- src/parse.c
+++ src/parse.c
@@ -11,7 +11,7 @@
 
 int create_db_header(int fd, struct dbheader_t **headerOut) {
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == -1) {
+    if (header == NULL) {
         printf("Malloc failed to create db header\n");
 
         return STATUS_ERROR;
@@ -34,7 +34,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
     }
 
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == -1) {
+    if (header == NULL) {
         printf("Malloc failed to create a db header\n");
 
         return STATUS_ERROR;
